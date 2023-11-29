1db417fb139c2d9a67967416f0080a69225058a8
Yusuf Satria Borneo <yusufsatriaborneo@gmail.com>
2023-11-29 20:40:18 +0800

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+const Home = () => {
+  return (
+    <div className=" px-5 py-6">
+      <h1 className=" font-extrabold text-4xl px-5 py-1">Home</h1>
+      <p className=" text-justify px-5 py-5">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        eius facere ullam quam, vero veritatis tempore fugiat nulla voluptatibus
+        doloremque eum eaque modi commodi, minus illo temporibus soluta nam aut
+        autem accusantium quaerat neque totam corrupti! Eos beatae, dolor neque
+        adipisci ut soluta dignissimos? Ex blanditiis aperiam suscipit facilis
+        harum architecto necessitatibus porro nesciunt tempore, facere quia
+        consequatur placeat. Non debitis saepe hic laborum dolorum eos
+        reprehenderit esse quam placeat! Natus harum incidunt dicta expedita
+        repellendus maxime adipisci fugiat, ducimus facere exercitationem
+        dolorem eveniet officia deserunt numquam consequuntur amet tempore
+        tempora! Corrupti sapiente, necessitatibus adipisci officia cupiditate
+        autem aliquam quae.
+      </p>
+    </div>
+  );
+};
+export default Home;
